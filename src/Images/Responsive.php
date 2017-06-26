8887dbdf8fb69076434928e3b7192dfad9288397
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-06-26 09:13:43 +0000

    added size and core classes to image responsive sh

--- src/Images/Responsive.php
+++ src/Images/Responsive.php
@@ -19,6 +19,8 @@ class Responsive extends Shortcake {
     
     public $defaults = array(
 	    'image_id' => '',
+        'alignment' => array(),
+        'class' => array(),	    
 	    'size' => '',
 	);
 
@@ -48,18 +50,46 @@ class Responsive extends Shortcake {
     			'frameTitle'  => esc_html__( 'Select Image', 'svbk-shortcakes' ),
     		),
     		array(
+    			'label'       => esc_html__( 'Alignment', 'svbk-shortcakes' ),
+    			'attr'        => 'alignment',
+    			'type'        => 'select',
+    			'options'     => array(
+    				array( 'value' => '', 'label' => esc_html__( 'None', 'svbk-shortcakes' ) ),
+    				array( 'value' => 'left', 'label' => esc_html__( 'Align Left', 'svbk-shortcakes' ) ),
+    				array( 'value' => 'center', 'label' => esc_html__( 'Align Center', 'svbk-shortcakes' ) ),
+    				array( 'value' => 'right', 'label' => esc_html__( 'Align Right', 'svbk-shortcakes' ) ),
+    			),
+    		),    		
+    		array(
     			'label'       => esc_html__( 'Size', 'svbk-shortcakes' ),
     			'attr'        => 'size',
     			'type'        => 'select',
     			'options'     => $sizes
-    		),    		
+    		),    
+        	array(
+        	    'label'  => esc_html__( 'Classes', 'svbk-shortcakes' ),
+        		'attr'   => 'class',
+        		'type'   => 'text',
+        	),       		
     	);
+    }
+    
+    protected function getClasses($attr){
+    
+        return array_merge( 
+            (array) $this->classes, 
+            $attr['alignment'] ? array( 'align' . $attr['alignment'] ) : array(),
+            $attr['size'] ? array( 'size-' . $attr['size'] ) : array(),
+            $attr['image_id'] ? array( 'wp-image-' . $attr['image_id'] ) : array(),
+            (array) $attr['class']
+        );
+    
     }
     
     function renderOutput( $attr, $content, $shortcode_tag ) {
     	$attr = $this->shortcode_atts( $this->defaults, $attr, $shortcode_tag );
 
-        $output['wrapperBegin'] = '<figure class="'.join(' ', $this->classes).'">';
+        $output['wrapperBegin'] = '<figure class="' . esc_attr( join( ' ', $this->getClasses($attr) ) ) . '">';
         $output['image'] = wp_get_attachment_image( $attr['image_id'], $attr['size'] );
         
         if($content){
