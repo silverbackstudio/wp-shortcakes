a2f3a03adda7a4d344a9d8fc314ce7e1c256b3cd
Brando Meniconi <amministrazione@silverbackstudio.it>
2017-12-19 13:18:53 +0000

    Remove link capabilities from Image

--- src/Images/Responsive.php
+++ src/Images/Responsive.php
@@ -4,9 +4,7 @@ namespace Svbk\WP\Shortcakes\Images;
 
 use Svbk\WP\Shortcakes\Shortcake;
 
-use Svbk\WP\Shortcakes\Content\Link;
-
-class Responsive extends Link {
+class Responsive extends Shortcake {
 
 	public $shortcode_id = 'responsive_image';
 	public $icon = 'dashicons-format-image';
@@ -30,11 +28,8 @@ class Responsive extends Link {
 	public static $defaults = array(
 		'image_id' => '',
 		'alignment' => '',
+		'link' => '',
 		'class' => '',
-		'link_post' => '',
-		'link_term' => '',
-		'link_url' => '',
-		'link_target' => 0,
 		'size' => '',
 	);
 
@@ -51,15 +46,7 @@ class Responsive extends Link {
 		return $args;
 
 	}
-	
-	public function remapFields(){
-		return array(
-			'post_id' => 'link_post',
-			'term' => 'link_term',
-			'url' => 'link_url',
-			'target' => 'link_target',
-		);
-	}
+
 
 	public function fields() {
 
@@ -76,56 +63,55 @@ class Responsive extends Link {
 				)
 			)
 		);
-
-		$link_fields = parent::fields();
-
-		foreach( $this->remapFields() as $old=>$new ){
-			$link_fields[$new] = $link_fields[$old];
-			$link_fields[$new]['attr'] = $new;
-			unset($link_fields[$old]);
-		}
 		
-		return array_merge(
-			array(
-				'image_id' => array(
-					'label'       => esc_html__( 'Image', 'svbk-shortcakes' ),
-					'attr'        => 'image_id',
-					'type'        => 'attachment',
-					'libraryType' => array( 'image' ),
-					'addButton'   => esc_html__( 'Select Image', 'svbk-shortcakes' ),
-					'frameTitle'  => esc_html__( 'Select Image', 'svbk-shortcakes' ),
-				),
-				'alignment' => array(
-					'label'       => esc_html__( 'Alignment', 'svbk-shortcakes' ),
-					'attr'        => 'alignment',
-					'type'        => 'select',
-					'options'     => array(
-						array(
-							'value' => '',
-							'label' => esc_html__( 'None', 'svbk-shortcakes' ),
-						),
-						array(
-							'value' => 'left',
-							'label' => esc_html__( 'Align Left', 'svbk-shortcakes' ),
-						),
-						array(
-							'value' => 'center',
-							'label' => esc_html__( 'Align Center', 'svbk-shortcakes' ),
-						),
-						array(
-							'value' => 'right',
-							'label' => esc_html__( 'Align Right', 'svbk-shortcakes' ),
-						),
+		return array(
+			'image_id' => array(
+				'label'       => esc_html__( 'Image', 'svbk-shortcakes' ),
+				'attr'        => 'image_id',
+				'type'        => 'attachment',
+				'libraryType' => array( 'image' ),
+				'addButton'   => esc_html__( 'Select Image', 'svbk-shortcakes' ),
+				'frameTitle'  => esc_html__( 'Select Image', 'svbk-shortcakes' ),
+			),
+			'alignment' => array(
+				'label'       => esc_html__( 'Alignment', 'svbk-shortcakes' ),
+				'attr'        => 'alignment',
+				'type'        => 'select',
+				'options'     => array(
+					array(
+						'value' => '',
+						'label' => esc_html__( 'None', 'svbk-shortcakes' ),
+					),
+					array(
+						'value' => 'left',
+						'label' => esc_html__( 'Align Left', 'svbk-shortcakes' ),
+					),
+					array(
+						'value' => 'center',
+						'label' => esc_html__( 'Align Center', 'svbk-shortcakes' ),
+					),
+					array(
+						'value' => 'right',
+						'label' => esc_html__( 'Align Right', 'svbk-shortcakes' ),
 					),
 				),
-				'size' => array(
-					'label'       => esc_html__( 'Size', 'svbk-shortcakes' ),
-					'attr'        => 'size',
-					'type'        => 'select',
-					'options'     => $sizes,
-				),
 			),
-			$link_fields
+			'link' => array(
+				'label'       => esc_html__( 'Link', 'svbk-shortcakes' ),
+				'attr'        => 'size',
+				'type'        => 'radio',
+				'options'     => array(
+					'' => esc_html__( 'None', 'svbk-shortcakes' ),
+					'image' => esc_html__( 'Image', 'svbk-shortcakes' ),
+					'attachment' => esc_html__( 'Attachment', 'svbk-shortcakes' ),
+				)
+			),			
+			'size' => array(
+				'label'       => esc_html__( 'Size', 'svbk-shortcakes' ),
+				'attr'        => 'size',
+				'type'        => 'select',
+				'options'     => $sizes,
+			),
 		);
 	}
 
@@ -149,20 +135,37 @@ class Responsive extends Link {
 
 	}
 
+	public function getLink( $attr ){
+		
+		$link = '';
+		
+		if( empty( $attr['link'] ) ) {
+			return '';
+		}
+		
+		switch( $attr['link'] ) {
+			case 'image': 
+				$link = wp_get_attachment_url( $attr['image_id'] );
+				break;
+			case 'attachment': 
+				$link = get_attachment_link( $attr['image_id'] );
+				break;
+		}
+		
+		return $link;
+	}
+
 	public function renderOutput( $attr, $content, $shortcode_tag ) {
 		$attr = $this->shortcode_atts( self::$defaults, $attr, $shortcode_tag );
 
-		$link_attr = $attr;
-		$remap = array_flip( $this->remapFields() );
-
-		foreach( $remap as $image => $link ){
-			$link_attr[$link] = $attr[$image];
-			unset($link_attr[$image]);
+		$output = parent::renderOutput($attr, $content, $shortcode_tag);
+		
+		$link = $this->getLink( $attr );
+		
+		if( $link ) {
+			$output['linkBegin'] = '<a class="image-link" href="' . esc_url( $link ) . '">';
+			$output['linkEnd'] = '</a>';
 		}
-
-		unset( $link_attr['class'] );
-
-		$output = parent::renderOutput($link_attr, $content, $shortcode_tag);
 
 		$output['wrapperBegin'] = '<figure ' . $this->renderClasses( $this->getClasses( $attr ) ) . '">';
 		$output['image'] = wp_get_attachment_image( $attr['image_id'], $attr['size'] );
